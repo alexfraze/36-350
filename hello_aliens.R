8485646f8404b04706ae40d443239dd9f15fa5c0
alex frazier <afrazie1@andrew.cmu.edu>
2017-12-10 12:54:47 -0500

    adding in hello aliens for 350 final project
    message to intelligent species from other planets

---
+++ hello_aliens.R
@@ -0,0 +1,10 @@
+cat("Hello living organisms from a different exoplanet. I am another living organism from Earth. 
+    Myself and the other living organisms earth would like to learn more about you and your species. 
+    What do you call your species? What forms of communication do you prefer to use? Do you have a common set of rules or laws that you abide by? 
+    What is neccessary for you to live and function? Do you have a specific purpose? 
+    
+    Hopefully we can establish a friendly and collaborative relationship between our two species. 
+    
+    Best,
+    Humankind")
+
